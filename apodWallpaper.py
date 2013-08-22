f75ffb7b26bd03c66a82f401dbde02923082b6f9
Patrick <patrick@server.fake>
2013-08-22 14:51:37 +0200

    Script now checks if Internet connection is available a couple of times

--- apodWallpaper.py
+++ apodWallpaper.py
@@ -4,6 +4,7 @@ import os
 import re
 import sys
 import subprocess
+import time
 import urlparse
 
 try:  import requests
@@ -11,15 +12,41 @@ except ImportError:
     sys.exit("'requests' library not found! Make sure it is installed.")
 try: import html2text
 except ImportError:
-    sys.exit("'html2text' library not found! Make sure it is installed.") 
+    sys.exit("'html2text' library not found! Make sure it is installed.")
+
 
 #  config vars
 apodURL = "http://apod.nasa.gov/apod/"
 downloadDIR = "~/Downloads/wallpapers/"
 
 
-#  replace ~ in downloadDIR 
+#  replace ~ in downloadDIR
 downloadDIR = os.path.expanduser(downloadDIR)
+
+def check_connection(url, trials):
+    """Checks if device is connected to the Internet
+    :returns: source of url
+
+    """
+    status = False
+    for n in range(1, trials+1):
+        try:
+            r = requests.get(url, timeout=3)
+            status = r.status_code
+            break
+        except requests.exceptions.Timeout:
+            continue
+        except requests.exceptions.ConnectionError:
+            if n != trials:
+                print "Connection error number %s! Waiting some seconds before retrying" % n
+                time.sleep(3)
+            continue
+
+    if status == 200:
+        return r.text
+    else:
+        print("Failed to retrieve data for the %s time" % n)
+        sys.exit("Cannot connect to the Internet! Check your connection.")
 
 #  screen dimensions
 dimsOut, _ = subprocess.Popen("xdpyinfo | grep dimensions", stdout=subprocess.PIPE, shell=True).communicate()
@@ -28,8 +55,11 @@ if dims == None:
     sys.exit("Cannot display resolution! Make sure xdpyinfo is installed.")
 width, height = map(int, dims.groups())
 
+#  check if computer is online
+print("Checking whether device is connected to the internet")
+
 #  get image src
-htmlSRC = requests.get(apodURL).text
+htmlSRC = check_connection(apodURL, 10)
 img = re.search('<a\s+href=\"([a-z]+\/[[0-9]{4}\/)([a-zA-Z0-9_]+.jpg)\">\s+<img\s+src=', htmlSRC, re.IGNORECASE)
 try:
     imgPATH, imgNAME = img.groups()
@@ -37,7 +67,7 @@ except AttributeError:
     sys.exit("Cannot find image on %s! Probably other media (video) available." % apodURL)
 imgLOC = os.path.join(downloadDIR, imgNAME)
 
-print("Location of image: %s\n" % imgLOC)
+print("Location of image: %s" % imgLOC)
 
 #  create download dir
 if not os.path.exists(downloadDIR):
@@ -50,7 +80,7 @@ if os.path.exists(imgLOC):
 #  download image
 r = requests.get(urlparse.urljoin(apodURL, imgPATH) + imgNAME, stream=True)
 if r.status_code == 200:
-    print("Downloading image\n")
+    print("Downloading image")
     with open(imgLOC, 'wb') as f:
         for chunk in r.iter_content(1024):
             f.write(chunk)
@@ -66,7 +96,7 @@ expH = int(height*.1)
 
 #  resize image
 new_height = height - expH
-print("Resizing image to %sx%s\n" % (new_height, width))
+print("Resizing image to %sx%s" % (new_height, width))
 _ = subprocess.Popen( "convert {0} -resize x{1} -size {2}x{1} xc:black +swap -gravity center -composite {0}".format(imgLOC, new_height, width), shell=True).communicate()
 
 #  add image description
